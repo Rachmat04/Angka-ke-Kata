a60da50fdcc7dc35249c0ce959f7f26d9b2b2de2
Rachmat04 <rachmatwahidi.site@gmail.com>
2016-11-12 12:54:53 +0700

    Create app.

--- Angka ke Terbilang/Program.cs
+++ Angka ke Terbilang/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ *  C# Program to Convert Digits to Words
+ *  The source code taken from http://www.sanfoundry.com/csharp-program-digits-words/, and modified by me
+ */
+using System;
 
-namespace Angka_ke_Terbilang
+public class UbahAngkakeKata
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        int num;
+        int nextdigit;
+        int numdigits;
+        int[] n = new int[20];
+
+        string[] digits = { "nol", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan" };
+
+
+        Console.WriteLine("Ketikkan angka");
+        Console.WriteLine();
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Angka yang Anda masukkan: " + num);
+        Console.Write("Dalam kata: ");
+        nextdigit = 0;
+        numdigits = 0;
+        do
         {
-        }
+            nextdigit = num % 10;
+            n[numdigits] = nextdigit;
+            numdigits++;
+            num = num / 10;
+        } while (num > 0);
+        numdigits--;
+        for (; numdigits >= 0; numdigits--)
+            Console.Write(digits[n[numdigits]] + " ");
+        Console.WriteLine();
+        Console.ReadLine();
     }
 }
