94cf070927a2b511943a7aa8317459bb01de39db
Rachmat04 <rachmatwahidi.site@gmail.com>
2016-11-12 12:47:36 +0700

    Add project files.

---
+++ Angka ke Terbilang/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angka_ke_Terbilang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
